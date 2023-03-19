7a37b591918d01e95031888c0a8c8c7540141d88
Mina Kim <rmaomina@gmail.com>
2023-03-19 22:36:03 +0900

    feat: environment variable setting

--- src/api.js
+++ src/api.js
@@ -1,14 +1,11 @@
+export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL
+export const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
 export const apiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '2465db124bmsh53a03126f0d8817p134aa5jsn17ef54e7d6e9',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    'X-RapidAPI-Key': process.env.REACT_APP_X_RapidAPI_Key,
+    'X-RapidAPI-Host': process.env.REACT_APP_X_RapidAPI_Host
   }
 };
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
-  
-
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-
-export const WEATHER_API_KEY = "fc877a769442c651374540a1b75cad6d"
